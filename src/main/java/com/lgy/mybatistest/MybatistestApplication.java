21c003e5d4693f48761f3ec7e4de69bb3f83eb73
刘杲阳 <liugaoyang_3@sina.com>
2018-12-23 23:48:57 +0800

    commit

---
+++ src/main/java/com/lgy/mybatistest/MybatistestApplication.java
@@ -0,0 +1,14 @@
+package com.lgy.mybatistest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MybatistestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MybatistestApplication.class, args);
+	}
+
+}
+
